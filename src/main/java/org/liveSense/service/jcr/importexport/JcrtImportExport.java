59b44384e65d0165bcf8d644fc0ae9a063dea4b6
Robert Csakany <csakany.robert@gmail.com>
2011-11-27 15:40:45 +0100

    Initial release

---
+++ src/main/java/org/liveSense/service/jcr/importexport/JcrtImportExport.java
@@ -0,0 +1,173 @@
+package org.liveSense.service.jcr.importexport;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
+
+import javax.jcr.ImportUUIDBehavior;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+
+import org.apache.felix.scr.annotations.Activate;
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Deactivate;
+import org.apache.felix.scr.annotations.Properties;
+import org.apache.felix.scr.annotations.Property;
+import org.apache.felix.scr.annotations.Reference;
+import org.apache.sling.commons.osgi.PropertiesUtil;
+import org.apache.sling.jcr.api.SlingRepository;
+import org.liveSense.service.jcr.importexport.webconsole.JcrImportExportWebConsolePlugin;
+import org.osgi.framework.BundleContext;
+import org.osgi.service.cm.ConfigurationAdmin;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+@Component(immediate=true, metatype=true)
+@Properties(value={
+		@Property(name=JcrtImportExport.PROP_BACKUP_DIR),
+		@Property(name=JcrtImportExport.PROP_WORKSPACE),
+		@Property(name=JcrtImportExport.PROP_START_PATH)
+})
+public class JcrtImportExport {
+	
+	public static final String PROP_BACKUP_DIR = "backuppath";
+	public static final String DEFAULT_BACKUP_DIR = "/backup";
+
+	public static final String PROP_WORKSPACE = "workspace";
+	public static final String DEFAULT_WORKSPACE = "default";
+
+	public static final String PROP_START_PATH = "startpath";
+	public static final String DEFAULT_START_PATH = "/";
+
+	
+	private static final Logger log = LoggerFactory.getLogger(JcrtImportExport.class);
+
+	@Reference
+	ConfigurationAdmin configAdmin;
+	
+	@Reference
+	SlingRepository repository;
+	
+	private String backupPath = DEFAULT_BACKUP_DIR;
+	private String workspace = DEFAULT_WORKSPACE;
+	private String startParth = DEFAULT_START_PATH;
+	
+
+	public static String exportRepository(SlingRepository repository, String workspace, String path, String startPath) throws Exception {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HH:mm:SS");
+		
+		if (startPath == null || "".equals(startPath))
+			startPath = "/";
+		if (workspace == null || "".equals(workspace))
+			workspace = repository.getDefaultWorkspace();
+		
+		log.info("Backuping repository:"+workspace+" ...");
+		Session session = null;
+		try {
+			session = repository.loginAdministrative(workspace);
+		} catch (RepositoryException e) {
+			log.error("Could not get session",e);
+			throw new Exception("Could not get JCR Session",e);
+		}
+		
+		String d = sdf.format(new Date());
+		String filepath = path+"/"+workspace+"-"+d+".xml.gz";
+		File f = new File(filepath);
+        if (f.exists()) {
+            throw new IllegalArgumentException("Export file "+filepath+" is existing, can not export");
+        }
+        try {
+            FileOutputStream os = new FileOutputStream(f);
+            GZIPOutputStream out = new 
+          		  GZIPOutputStream(new BufferedOutputStream(os));
+            
+            //export all including binary, recursive
+            session.exportDocumentView(startPath, out, false, false);
+            out.flush();
+            out.close();
+            os.close();
+    		log.info("Backuping repository: "+workspace+"... OK");
+
+        } catch(Throwable t) {
+        	log.error("Failed to export repository at " +
+                "/" + " to file "+filepath+"\n", t);
+			throw new Exception("Failed to export repository at " +
+                "/" + " to file "+filepath+"\n",t);
+        }
+		session.logout();
+		return filepath;
+	}
+
+	
+	public static String importRepository(SlingRepository repository, String workspace, String path, String startPath) throws Exception {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HH:mm:SS");
+		
+		if (startPath == null || "".equals(startPath))
+			startPath = "/";
+		if (workspace == null || "".equals(workspace))
+			workspace = repository.getDefaultWorkspace();
+		
+		log.info("Restoring repository:"+workspace+" ...");
+		Session session = null;
+		try {
+			session = repository.loginAdministrative(workspace);
+		} catch (RepositoryException e) {
+			log.error("Could not get session",e);
+			throw new Exception("Could not get JCR Session",e);
+		}
+		
+		String d = sdf.format(new Date());
+		String filepath = path;
+		File f = new File(filepath);
+        if (!f.exists()) {
+            throw new IllegalArgumentException("Import file "+filepath+" is existing, can not import");
+        }
+        try {
+        	FileInputStream fis = new FileInputStream(f);
+            GZIPInputStream gs = new GZIPInputStream(fis);
+        	            
+            //import all
+            session.importXML(startPath, gs, ImportUUIDBehavior.IMPORT_UUID_COLLISION_REPLACE_EXISTING );
+            gs.close();
+            fis.close();
+    		log.info("Restoring repository: "+workspace+"... OK");
+
+        } catch(Throwable t) {
+        	log.error("Failed to import repository at " +
+                "/" + " from file "+filepath+"\n", t);
+			throw new Exception("Failed to import repository at " +
+                "/" + " from file "+filepath+"\n",t);
+        }
+		session.logout();
+		return filepath;
+	}
+
+	
+	JcrImportExportWebConsolePlugin plugin = null;
+	
+	@Activate
+	protected void activate(BundleContext context) {
+
+		backupPath = PropertiesUtil.toString(context.getProperty(PROP_BACKUP_DIR), context.getProperty("sling.home"));
+		workspace = PropertiesUtil.toString(context.getProperty(PROP_WORKSPACE), context.getProperty(JcrtImportExport.DEFAULT_WORKSPACE));
+		startParth = PropertiesUtil.toString(context.getProperty(PROP_START_PATH), context.getProperty(JcrtImportExport.DEFAULT_START_PATH));
+
+		plugin = new JcrImportExportWebConsolePlugin(context, repository, backupPath, workspace, startParth);
+	}
+
+	@Deactivate
+	protected void deactivate(BundleContext context) {
+
+	    if (plugin != null) {
+	        plugin.dispose();
+	        plugin = null;
+	    }
+	}
+
+}
---
+++ src/main/java/org/liveSense/service/jcr/importexport/webconsole/JcrImportExportWebConsolePlugin.java
@@ -0,0 +1,268 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.liveSense.service.jcr.importexport.webconsole;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.Dictionary;
+import java.util.Hashtable;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.sling.jcr.api.SlingRepository;
+import org.liveSense.service.jcr.importexport.JcrtImportExport;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.Constants;
+import org.osgi.framework.ServiceRegistration;
+
+public class JcrImportExportWebConsolePlugin extends
+        HttpServlet {
+
+    private static final long serialVersionUID = 0;
+
+    private static final String ATTR_SUBMIT = "plugin.submit";
+
+    private static final String PAR_WORKSPACE= "repository";
+    private static final String PAR_DEFAULT_PATH = "backuppath";
+    private static final String PAR_START_PATH = "startpath";
+
+    private static final String PAR_MSG = "msg";
+
+    private transient ServiceRegistration service;
+    SlingRepository repository;
+    
+    String defaultPath;
+    String defaultWorkspace;
+    String defaultStartPath;
+    
+    public JcrImportExportWebConsolePlugin(BundleContext context,
+            SlingRepository repository, String defaultPath, String defaultWorkspace, String defaultStartPath) {
+        this.repository = repository;
+        this.defaultPath = defaultPath;
+        this.defaultWorkspace = defaultWorkspace;
+        this.defaultStartPath = defaultStartPath;
+        
+        Dictionary<String, Object> props = new Hashtable<String, Object>();
+        props.put(Constants.SERVICE_DESCRIPTION,
+            "JCRResourceResolver Web Console Plugin");
+        props.put(Constants.SERVICE_VENDOR, "liveSense.org");
+        props.put(Constants.SERVICE_PID, getClass().getName());
+        props.put("felix.webconsole.label", "jcrimportexport");
+        props.put("felix.webconsole.title", "liveSense JCR Import/Export");
+        props.put("felix.webconsole.configprinter.modes", "always");
+
+        service = context.registerService(new String[] {
+                "javax.servlet.Servlet" },
+            this, props);
+    }
+
+    public void dispose() {
+        if (service != null) {
+            service.unregister();
+            service = null;
+        }
+    }
+
+    
+    private String getParameterValue(HttpServletRequest req, String key, String defaultValue) {
+    	if (req.getParameter(key) == null ||  "".equals(req.getParameter(key))) {
+    		return defaultValue;
+    	} else {
+    		return req.getParameter(key);
+    	}
+    }
+    
+    @Override
+    protected void doGet(final HttpServletRequest request, final HttpServletResponse response)
+    throws ServletException, IOException {
+
+        final String backupPath = getParameterValue(request, PAR_DEFAULT_PATH, defaultPath);
+        final String workspace = getParameterValue(request, PAR_WORKSPACE, defaultWorkspace);
+        final String startPath = getParameterValue(request, PAR_START_PATH, defaultStartPath);
+        final String msg = getParameterValue(request, PAR_MSG, null);
+        
+        final PrintWriter pw = response.getWriter();
+
+        
+        if (msg != null) {
+            pw.println("<table class='content' cellpadding='0' cellspacing='0' width='100%'>");
+        	titleHtml(pw,
+                    "Message",
+                    msg);
+            pw.println("</table>");
+
+        }
+        
+        pw.print("<form method='post'>");
+
+        pw.println("<table class='content' cellpadding='0' cellspacing='0' width='100%'>");
+
+        titleHtml(
+            pw,
+            "Export Workspace",
+            "To export a workspace you have specify Backup path, workspace and the start path in repository." +
+            "The two main repository you can use \"default\" and \"security\"" +
+            "The generated file name will be <workspacename>-<timestamp>.xml.gz");
+
+        pw.println("<tr class='content'>");
+        pw.println("<td class='content'>Default path</td><td class='content' colspan='2'>");
+        pw.println("<input type='text' name='" + PAR_DEFAULT_PATH + "' value='" + (backupPath != null ? backupPath : "")
+            + "' class='input' size='100'/>");
+
+        pw.println("</td></tr><tr class='content'><td class='content'>Workspace</td><td class='content' colspan='2'>");
+        
+        pw.println("<input type='text' name='" + PAR_WORKSPACE + "' value='" + (workspace != null ? workspace : "")
+                + "' class='input' size='50'/>");
+        
+        pw.println("</td></tr><tr class='content'><td class='content'>Start path</td><td class='content' colspan='2'>");
+        
+        pw.println("<input type='text' name='" + PAR_START_PATH + "' value='" + (startPath != null ? startPath : "")
+                + "' class='input' size='100'/>");
+        pw.println("</td></tr>");
+        
+        separatorHtml(pw);
+        
+        pw.println("<tr class='content'><td class='content'></td><td class='content' colspan='2'>");
+        pw.println("<input type='submit' name='" + ATTR_SUBMIT
+            + "' value='Export' class='submit'/>");
+        
+        pw.print("</td>");
+        pw.println("</tr>");
+        pw.print("</form>");
+
+        pw.println("</table>");
+
+    
+        pw.print("<form method='post'>");
+
+        pw.println("<table class='content' cellpadding='0' cellspacing='0' width='100%'>");
+
+        titleHtml(
+            pw,
+            "Import Workspace",
+            "To import a workspace you have specify backup file, a workspace import to and the start path in repository.");
+
+        pw.println("<tr class='content'>");
+        pw.println("<td class='content'>File name</td><td class='content' colspan='2'>");
+        pw.println("<input type='text' name='" + PAR_DEFAULT_PATH + "' value='" + (backupPath != null ? backupPath : "")
+            + "' class='input' size='100'/>");
+
+        pw.println("</td></tr><tr class='content'><td class='content'>Workspace</td><td class='content' colspan='2'>");
+        
+        pw.println("<input type='text' name='" + PAR_WORKSPACE + "' value='" + (workspace != null ? workspace : "")
+                + "' class='input' size='50'/>");
+        
+        pw.println("</td></tr><tr class='content'><td class='content'>Start path</td><td class='content' colspan='2'>");
+        
+        pw.println("<input type='text' name='" + PAR_START_PATH + "' value='" + (startPath != null ? startPath : "")
+                + "' class='input' size='100'/>");
+        
+        pw.println("</td></tr>");
+        
+        separatorHtml(pw);
+        
+        pw.println("<tr class='content'><td class='content'></td><td class='content' colspan='2'>");
+
+        pw.println("<input type='submit' name='" + ATTR_SUBMIT
+            + "' value='Import' class='submit'/>");
+        
+        pw.print("</td>");
+        pw.println("</tr>");
+        pw.print("</form>");
+
+
+        pw.println("</table>");
+
+    
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request,
+            HttpServletResponse response) throws ServletException, IOException {
+
+        final String backupPath = getParameterValue(request, PAR_DEFAULT_PATH, defaultPath);
+        final String workspace = getParameterValue(request, PAR_WORKSPACE, defaultWorkspace);
+        final String startPath = getParameterValue(request, PAR_START_PATH, defaultStartPath);
+        String msg = null;
+
+        if ("Export".equals(request.getParameter(ATTR_SUBMIT))) {
+        	try {
+        		msg = "Export finsihed successfully: "+JcrtImportExport.exportRepository(repository, workspace, backupPath, startPath);
+        	} catch (Exception e) {
+        		msg = e.getMessage();
+        	}
+        } else if ("Import".equals(request.getParameter(ATTR_SUBMIT))) {
+        	try {
+        		msg = "Import finsihed successfully: "+JcrtImportExport.importRepository(repository, workspace, backupPath, startPath);
+        	} catch (Exception e) {
+        		msg = e.getMessage();
+        	}
+        }
+    	
+        // finally redirect
+        final String path = request.getContextPath() + request.getServletPath() + request.getPathInfo();
+        final String redirectTo;
+        if ( msg == null ) {
+            redirectTo = path;
+        } else {
+            redirectTo = path + '?' + PAR_MSG + '=' + encodeParam(msg);
+        }
+        response.sendRedirect(redirectTo);
+    }
+
+    private String encodeParam(final String value) {
+        try {
+            return URLEncoder.encode(value, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            // should never happen
+            return value;
+        }
+    }
+
+    private void titleHtml(PrintWriter pw, String title, String description) {
+        pw.println("<tr class='content'>");
+        pw.println("<th colspan='3'class='content container'>" + title
+            + "</th>");
+        pw.println("</tr>");
+
+        if (description != null) {
+            pw.println("<tr class='content'>");
+            pw.println("<td colspan='3'class='content'>" + description
+                + "</th>");
+            pw.println("</tr>");
+        }
+    }
+
+    private void separatorHtml(PrintWriter pw) {
+        pw.println("<tr class='content'>");
+        pw.println("<td class='content' colspan='3'>&nbsp;</td>");
+        pw.println("</tr>");
+    }
+
+    private void separatorText(PrintWriter pw) {
+        pw.println();
+    }
+
+
+}
